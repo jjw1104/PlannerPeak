81d9db43700efea9370aeae98f130abba2532913
Jeremy Wizenfeld <jwizenfeld2022@yula.org>
2020-05-22 15:24:14 -0700

    Added models to notes

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -1,6 +1,17 @@
+#always run the following lines of code in terminal after every use
+# python manage.py makemigrations 
+# python manage.py migrate
+
+
 from django.db import models
 
 # Create your models here.
 
 class Note(models.Model):
     content = models.TextField()
+
+class Assignment(models.Model):
+    Name = models.TextField()
+    Class = models.TextField()
+    DueDate = models.DateField()
+    Description = models.TextField()
